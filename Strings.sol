035ac8eaa83c9687304de45f73e1712a2af77146
rabbit0222 <phamtranminhconghh92@gmail.com>
2023-01-23 16:30:23 +0000

    init repo

---
+++ Strings.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+// Inspired by https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
+
+pragma solidity ^0.6.0;
+
+library Strings {
+    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) internal pure returns (string memory) {
+        return string(abi.encodePacked(_a, _b, _c, _d, _e));
+    }
+
+    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) internal pure returns (string memory) {
+        return strConcat(_a, _b, _c, _d, "");
+    }
+
+    function strConcat(string memory _a, string memory _b, string memory _c) internal pure returns (string memory) {
+        return strConcat(_a, _b, _c, "", "");
+    }
+
+    function strConcat(string memory _a, string memory _b) internal pure returns (string memory) {
+        return strConcat(_a, _b, "", "", "");
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+}
